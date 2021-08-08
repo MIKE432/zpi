a274354d17ed932bd7385df37e4878d279188797
Szymon <s.zmijewski@campus.fct.unl.pt>
2021-08-08 20:52:22 +0200

    project setup

---
+++ spring-backend/src/main/kotlin/com/example/sztoswro/SztosWroApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sztoswro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SztosWroApplication
+
+fun main(args: Array<String>) {
+    runApplication<SztosWroApplication>(*args)
+}
