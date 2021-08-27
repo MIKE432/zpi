a36e038c9d1f18c94fef00b47512f517e39bf5c3
Szymon <s.zmijewski@campus.fct.unl.pt>
2021-08-27 15:35:24 +0200

    basic project configuration

--- spring-backend/src/main/kotlin/com/example/sztoswro/SztosWroApplication.kt
+++ spring-backend/src/main/kotlin/com/example/sztoswro/SztosWroApplication.kt
@@ -1,9 +1,10 @@
 package com.example.sztoswro
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = arrayOf(DataSourceAutoConfiguration::class))
 class SztosWroApplication
 
 fun main(args: Array<String>) {
---
+++ spring-backend/src/main/kotlin/com/example/sztoswro/security/CustomUserDetailService.kt
@@ -0,0 +1,2 @@
+package com.example.sztoswro.security
+
